46864313bd05517232a7b0d1a68ee18a597f606c
Gabriel Kenji Utiyama <gabrielkenji2000@hotmail.com>
2021-09-15 02:33:04 -0300

    pagina de agendamento

--- js/calendario.js
+++ js/calendario.js
@@ -1,52 +1,74 @@
 now = new Date();
 
-
 var query = location.search.slice(1);
-      var partes = query.split('&');
-      var data = {};
-      partes.forEach(function (parte) {
-        var chaveValor = parte.split('=');
-        var chave = chaveValor[0];
-        var valor = chaveValor[1];
-        data[chave] = valor;
-      })
-      document.addEventListener("DOMContentLoaded", function () {
-        var calendarEl = document.getElementById("calendar");
+var partes = query.split("&");
+var data = {};
+partes.forEach(function (parte) {
+  var chaveValor = parte.split("=");
+  var chave = chaveValor[0];
+  var valor = chaveValor[1];
+  data[chave] = valor;
+});
 
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          selectable: true,
-          height: 650,
-          locale: "pt-br",
-          axisFormat: "h:mm",
-          columnFormat: {
-            month: "ddd", // Mon
-            week: "ddd d", // Mon 7
-            day: "dddd M/d", // Monday 9/7
-            agendaDay: "dddd d",
-          },
-          buttonText: {
-            prev: "Anterior",
-            next: "Próximo",
-            today: "Hoje",
-            month: "Mês",
-            week: "Semana",
-            day: "Dia",
-            list: "Lista",
-          },
-          headerToolbar: {
-            left: "prev,next today",
-            center: "title",
-          },
-          // theme: true,
-          dateClick: function (info) {
-            alert("clicked " + info.dateStr);
 
-            DIA(info.dateStr, data.sede);
-            listar();
-          },
-        });
 
-        calendar.render();
-      });
-    
-      
+// document.addEventListener("DOMContentLoaded", function () {
+//   var calendarEl = document.getElementById("calendar");
+
+//   var calendar = new FullCalendar.Calendar(calendarEl, {
+//     selectable: true,
+//     height: "500px",
+//     expandRows: false,
+//     locale: "pt-br",
+//     axisFormat: "h:mm",
+//     columnFormat: {
+//       month: "ddd", // Mon
+//       week: "ddd d", // Mon 7
+//       day: "dddd M/d", // Monday 9/7
+//       agendaDay: "dddd d",
+//     },
+//     buttonText: {
+//       today: "Hoje",
+//       month: "Mês",
+//       week: "Semana",
+//       day: "Dia",
+//       list: "Lista",
+//     },
+//     headerToolbar: {
+//       left: "prev",
+//       center: "title",
+//       right: "next",
+//     },
+//     // theme: true,
+//     dateClick: function (info) {
+//       alert("clicked " + info.dateStr);
+
+//       DIA(info.dateStr, data.sede);
+//       listar();
+//     },
+//   });
+
+//   calendar.render();
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    height: '400px',
+    expandRows: false,
+    locale: "pt-br",
+    headerToolbar: {
+      left: 'prev',
+      center: 'title',
+      right: 'next'
+    },
+    selectable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+    dateClick: function (info) {
+                  DIA(info.dateStr, data.sede);
+                }
+  });
+
+  calendar.render();
+});
--- js/calendario_dados.js
+++ js/calendario_dados.js
@@ -7,21 +7,82 @@ function Agendar() {
   var dateField = document.getElementById("date");
   var date = dateField.value;
   var estacaoId = 4;
-  axios
-    .post("http://localhost:5000/agendamentos", { id, date, estacaoId })
-    .then((res) => {
-      DIA(date,1);
-      alert("criado");
-    })
-    .catch((err) => {
-      alert("falha no login");
-    });
+
+  var form = document.getElementById("dados");
+
+  
+    form.submit();
+  
+  
 }
 
+function Agenda(data,sede) {
 
 
+  axios
+    .get("http://localhost:5000/agendamentos/"+data+"/"+ sede )
+    .then((response) => {
+      var agendamentos = response.data;
+      var title_date = document.getElementById("title-date");
+      
+      var modal = document.getElementById("vaga");
+      modal.innerText = "";
+      
+      arruma_data = data.split("-");
+      title_date.innerHTML =
+        arruma_data[2] + "/" + arruma_data[1] + "/" + arruma_data[0];
+      agendar = false;
+      if (arruma_data[0] == now.getFullYear()) {
+        if (arruma_data[1] == now.getMonth() + 1) {
+          if (arruma_data[2] >= now.getDate()) {
+            agendar = true;
+          }
+        } else if (arruma_data[1] > now.getMonth()) {
+          agendar = true;
+        }
+      } else if (arruma_data[0] > now.getFullYear()) {
+        agendar = true;
+      }
 
-function DIA(data,sede) {
+      est = ""
+      select = false
+      agendamentos.estaco.forEach(estacoes => {
+        if(!select){
+          est = `<select class="form-control mb-3" id="exampleFormControlSelect1">`
+        }
+        select = true
+        est = est + `<option value="${estacoes.id}">${estacoes.number}</option>`
+      });
+      if(select){
+        est = est + "</select>"
+      }
+      
+
+      
+     
+      
+      if (agendar) {
+        modal.innerHTML =  `
+            
+                  <div class="progress mb-4">
+                  
+                  <div class="progress-bar" role="progressbar" style="width: 25%;"  aria-valuenow="30" aria-valuemin="0" aria-valuemax="240">30/240</div>
+                  </div>
+                  <h5>Escolha a sua estação de trabalho:</h5>
+                  ${est}
+                  <input type="hidden" name="id" id="id" value="3" >
+                  <input type="hidden" name="date" id="date" value=${data}>
+                  <button onclick="Agendar()">agendar</button>
+                  `;
+                  
+      }  
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+}
+function Lista(data,sede) {
   // document.getElementById('lista').innerHTML = "";
   // var $lista = document.getElementById('lista'),
   // // Pega a string do conteúdo atual
@@ -42,7 +103,7 @@ function DIA(data,sede) {
       var agendamentos = response.data;
       var title_date = document.getElementById("title-date");
       
-      var modal = document.getElementById("modal");
+      var modal = document.getElementById("vaga");
       modal.innerText = "";
 
       modal.innerHTML = `
@@ -82,19 +143,17 @@ function DIA(data,sede) {
         est = est + "</select>"
       }
       
+
+      
+     
       
       if (agendar) {
         modal.innerHTML =  `
             
                   <div class="progress mb-4">
                   
-                  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="30" aria-valuemin="0" aria-valuemax="240">30/240</div>
+                  <div class="progress-bar" role="progressbar" style="width: 25%;"  aria-valuenow="30" aria-valuemin="0" aria-valuemax="240">30/240</div>
                   </div>
-                  ${est}
-                  <input type="hidden" name="id" id="id" value="3" >
-                  <input type="hidden" name="date" id="date" value=${data}>
-                  <button onclick="Agendar()">agendar</button>
-                  
                   <table id="lista">
                   <tr>
                     <th>ID</th>
@@ -120,6 +179,173 @@ function DIA(data,sede) {
     .catch((error) => {
       console.log(error);
     });
+}
+
+
+
+function DIA(data,sede) {
+  axios
+    .get("http://localhost:5000/agendamentos/"+data+"/"+ sede )
+    .then((response) => {
+      var agendamentos = response.data;
+      
+      
+      var modal = document.getElementById("vaga");
+      var avancar = document.getElementById("avancar");
+      modal.innerText = "";
+
+      var livres = 0
+      agendamentos.estaco.forEach(estacoes => {
+        livres++
+      })
+
+      
+      var total = 3
+
+      
+      var ocupados = total - livres
+     
+      var por_atual = ocupados/(total/100)
+      arruma_data = data.split("-");
+       
+
+      modal.innerHTML =  `
+                
+              <div class="titulo-horario">
+                <h4 id>Data: ${arruma_data[2]}/${arruma_data[1]}/${arruma_data[0]}</h4>
+              </div>           
+                <div class="row quantidades">
+                <div class="col-md-12 quantidade">
+                  <label class="ocupacao"><b>Não é mais possivel agendar uma estação de trabalho nesse dia</b></label>
+                </div>
+              </div>
+                  `;
+
+                  avancar.innerHTML=""
+      
+      agendar = false;
+      if (arruma_data[0] == now.getFullYear()) {
+        if (arruma_data[1] == now.getMonth() + 1) {
+          if (arruma_data[2] >= now.getDate()) {
+            agendar = true;
+          }
+        } else if (arruma_data[1] > now.getMonth()) {
+          agendar = true;
+        }
+      } else if (arruma_data[0] > now.getFullYear()) {
+        agendar = true;
+      }
+
+      
+      
+      
+      if (agendar) {
+        if(total == ocupados){
+          modal.innerHTML =  `
+                
+              <div class="titulo-horario">
+                <h4 id>Data: ${arruma_data[2]}/${arruma_data[1]}/${arruma_data[0]}</h4>
+              </div>           
+                <div class="row quantidades">
+                <div class="col-md-12 quantidade">
+                  <label class="ocupacao"><b>Não há mais vagas disponiveis para este dia</b></label>
+                </div>
+              </div>
+                  `;
+                  avancar.innerHTML=""
+        }else{
+          modal.innerHTML =  `
+          <div class="titulo-horario">
+          <h4 class="mb-3">Data: ${arruma_data[2]}/${arruma_data[1]}/${arruma_data[0]}</h4>
+          <h5>Escolha o horários <br> 
+            entre 8h -18h </h5>
+        </div>
+        <form action="confirmacao.html" method="get" id="dados">
+        <div class="col-sm-12 ">
+          <div class="row horarios">
+            <div class="col-md-6  Entrada">
+              <h5>Entrada</h5>
+              
+              <input type="hidden" name="id" id="id" value="3" >
+              <input type="hidden" name="date" id="date" value=${data}>
+              <input type="hidden" name="sede" id="date" value=${sede}>
+              
+              <div class="horario-button">
+                <select class="form-control  horario-button" name="entrada"  onfocus='this.size=3;' onblur='this.size=1;' 
+                onchange='this.size=1; this.blur();'>
+                  <option value="8h00">8:00</option>
+                    <option value="8h30">8:30</option>
+                    <option value="9h00">9:00</option>
+                    <option value="9h30">9:30</option>
+                    <option value="10h00">10:00</option>
+                    <option value="10h30">10:30</option>
+                    <option value="11h00">11:00</option>
+                    <option value="11h30">11:30</option>
+                    <option value="12h00">12:00</option>
+                    <option value="12h30">12:30</option>
+                    <option value="13h00">13:00</option>
+                    <option value="13h30">13:30</option>
+                    <option value="14h00">14:00</option>
+                    <option value="14h30">14:30</option>
+                    <option value="15h00">15:00</option>
+                    <option value="15h30">15:30</option>
+                    <option value="16h00">16:00</option>
+                    <option value="16h30">16:30</option>
+                    <option value="17h00">17:00</option>
+                    <option value="17h30">17:30</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-6 Saida">
+              <h5>Saída</h5>
+              <div class="horario-button">
+                <select class="form-control horario-button" name="saida" onfocus='this.size=3;' onblur='this.size=1;' 
+                onchange='this.size=1; this.blur();'>
+                  <option value="8h30">8:30</option>
+                  <option value="9h00">9:00</option>
+                  <option value="9h30">9:30</option>
+                  <option value="10h00">10:00</option>
+                  <option value="10h30">10:30</option>
+                  <option value="11h00">11:00</option>
+                  <option value="11h30">11:30</option>
+                  <option value="12h00">12:00</option>
+                  <option value="12h30">12:30</option>
+                  <option value="13h00">13:00</option>
+                  <option value="13h30">13:30</option>
+                  <option value="14h00">14:00</option>
+                  <option value="14h30">14:30</option>
+                  <option value="15h00">15:00</option>
+                  <option value="15h30">15:30</option>
+                  <option value="16h00">16:00</option>
+                  <option value="16h30">16:30</option>
+                  <option value="17h00">17:00</option>
+                  <option value="17h30">17:30</option>
+                  <option value="18h00">18:00</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        </form>
+        <div class="row quantidades">
+          <div class="col-md-12 quantidade">
+            <label class="ocupacao"><b>${ocupados}</b> pessoas já agendaram </br>
+              para esse dia e horário, </br>
+              restam <b>${total - ocupados}</b> vagas</label>
+          </div>
+        </div>
+        `;
+
+        avancar.innerHTML=`<button onclick="Agendar()">Avançar</button>`
+        
+        }
+       
+                  
+      }
+
+
+    })
 }
 
 
